29de6af442fcbd6aede9fef5e60d8b541f7fc3a1
acidicMercury8 <acidicmercury8@outlook.com>
2025-03-16 14:29:33 +0300

    Enable correct parsing X-Ray specific comments
    Co-authored-by: Mortan <cheatmaster1@mail.ru>

--- src/lj_lex.c
+++ src/lj_lex.c
@@ -383,6 +383,31 @@ static LexToken lex_scan(LexState *ls, TValue *tv)
       }
     case LEX_EOF:
       return TK_eof;
+    case '/':
+      lex_next(ls);
+      if (ls->c == '/') {
+        while (ls->c != '\n' && ls->c != LEX_EOF) {
+          lex_next(ls);
+        }
+        continue;
+      }
+      else if (ls->c == '*') {
+        lex_next(ls);
+        while (ls->c != LEX_EOF) {
+          if (ls->c == '*') {
+            lex_next(ls);
+            if (ls->c == '/') {
+              lex_next(ls);
+              break;
+            }
+          }
+          lex_next(ls);
+        }
+        continue;
+      }
+      else {
+        return '/';
+      }
     default: {
       LexChar c = ls->c;
       lex_next(ls);
